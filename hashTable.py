c8d48053496d8c067235c0f9d3d53bdbcf95724e
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-04-28 12:43:08 +0545

    hash maped

--- hashTable.py
+++ hashTable.py
@@ -1,6 +1,23 @@
-def get_hash_key(key):
-    h=0 
-    for char in key:
+
+class HashTable:
+    def __init__(self):
+        
+        self.MAX = 100
+        self.arr = [ None for i in range(self.MAX)]
+
+    def get_hash_key(self,key):
+            h=0 
+        for char in key:
         h+=ord(char)
-    return h %10
+         return h %self.MAX
+    def __setitem__(self,key,value):
+        h= self.get_hash_key(key)
+        self.arr[h]=value
+    def __getitem__(key,value):
+        h =get_hash_key(key)
+        return self.arr[h]
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None  
     
+
