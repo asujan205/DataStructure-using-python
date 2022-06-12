5f1c90e460b8bdc1ab75312fe130d0eb62413e8f
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-06-12 21:41:54 +0545

    general tree

--- tree.py
+++ tree.py
@@ -1,16 +1,35 @@
 class Treenode:
     def __init__(self,data):
         self.data=data
-        self.childern=[]
+        self.children=[]
         self.parent=None
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+
+        return level
+
+    def print_tree(self):
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
     def add_childs(self,child):
         child.parent=self
-        self.childern.append(child)
+        self.children.append(child)
 def build_tree():
     root=Treenode("sujan")
-    laptop=Treenode("acer")
-    root.add_childs(laptop)
-    return root
+    laptop1=Treenode("acer")
+    laptop2=Treenode("lenevo")
+    root.add_childs(laptop1)
+    root.add_childs(laptop2)
+    laptop2.add_childs(Treenode("kiran"))
+    root.print_tree()
 if __name__=='__main__':
     root=build_tree()
     print(root)
