ab60057058e17ae0c3d586b598a999fa9ed3d36a
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-06-12 23:25:29 +0545

    binarytree

---
+++ binarytree.py
@@ -0,0 +1,24 @@
+class BinarySearchTreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+    def add_child(self,data):
+        if data ==self.data:
+            return
+        if data<self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left=BinarySearchTreeNode(data)
+    
+        else:
+            if self.right:
+                self.left.add_child(data)
+            else:
+                self.right=BinarySearchTreeNode(data)
+    def in_order_traversal(self):
+        elements=[]
+        if self.left:
+            elements+=self.left.in_order_traversal()
+        return elements
