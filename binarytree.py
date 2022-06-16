46a80350e3312e26cb439fc364ebbee3abe1cd6f
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-06-16 13:56:25 +0545

    search is done

--- binarytree.py
+++ binarytree.py
@@ -1,40 +1,69 @@
-from distutils.command.build import build
-
-
 class BinarySearchTreeNode:
-    def __init__(self,data):
-        self.data=data
-        self.left=None
-        self.right=None
-    def add_child(self,data):
-        if data ==self.data:
-            return
-        if data<self.data:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return # node already exist
+
+        if data < self.data:
             if self.left:
                 self.left.add_child(data)
             else:
-                self.left=BinarySearchTreeNode(data)
-    
+                self.left = BinarySearchTreeNode(data)
         else:
             if self.right:
-                self.left.add_child(data)
+                self.right.add_child(data)
             else:
-                self.right=BinarySearchTreeNode(data)
+                self.right = BinarySearchTreeNode(data)
+
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
     def in_order_traversal(self):
-        elements=[]
+        elements = []
         if self.left:
-            elements+=self.left.in_order_traversal()
+            elements += self.left.in_order_traversal()
+
         elements.append(self.data)
+
         if self.right:
-            elements+=self.right.in_order_traversal()
+            elements += self.right.in_order_traversal()
 
         return elements
+
+
 def build_tree(elements):
+    print("Building tree with these elements:",elements)
     root = BinarySearchTreeNode(elements[0])
+
     for i in range(1,len(elements)):
         root.add_child(elements[i])
-    return root
-if __name__ == '__main__':
-    numbers=[1,2,3,4,34,78,32,90,17,30]
-    numbers_tree=build_tree(numbers)
 
+    return root
+
+if __name__ == '__main__':
+    countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
+    country_tree = build_tree(countries)
+
+    print("UK is in the list? ", country_tree.search("UK"))
+    print("Sweden is in the list? ", country_tree.search("Sweden"))
+
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    print("In order traversal gives this sorted list:",numbers_tree.in_order_traversal())
